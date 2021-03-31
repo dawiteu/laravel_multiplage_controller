d77a3545a8e7c30b58c0a9d9e3d0bfbb341d7195
dawiteu <dawid@tararuj.be>
2021-03-31 12:00:48 +0200

    preparation terrain 100%

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Business Casual - Start Bootstrap Theme</title>
+
+    <link rel="stylesheet" href={{asset('css/business-casual.css')}}>
+    <link href={{asset('vendor/bootstrap/css/bootstrap.min.css')}} rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+
+    <link rel="stylesheet" href={{asset('css/business-casual.min.css')}}>
+
+
+</head>
+<body>
+    
+
+    <script src={{asset('js/app.js')}}></script>
+</body>
+</html>
