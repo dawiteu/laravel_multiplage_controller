8e7207d15debedc6ee129d9e65dd5b454a02962b
dawiteu <dawid@tararuj.be>
2021-03-31 17:07:37 +0200

    fin

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -30,9 +30,7 @@ class StoreController extends Controller
             'text9'=>'Founded in 1987 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.',
             'text10'=>'We guarantee that you will fall in
             <em>lust</em>;
-            with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.',
-            ''=>'',
-
+            with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.'
         ];
         return view('pages.store', compact('informations'));
     }
